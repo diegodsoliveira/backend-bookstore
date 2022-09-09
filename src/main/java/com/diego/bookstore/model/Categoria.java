97572163faa065bcc04dd911007dc57b6f1f385a
Diego <diegodsoliveira@gmail.com>
2022-09-09 15:38:19 -0300

    classes models iniciadas

---
+++ src/main/java/com/diego/bookstore/model/Categoria.java
@@ -0,0 +1,81 @@
+package com.diego.bookstore.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = -5303364689501973300L;
+
+    private Integer id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria(Integer id, String titulo, String nomeAutor, String texto) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+    }
+
+    public Categoria() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(getId(), categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/diego/bookstore/model/Livro.java
@@ -0,0 +1,72 @@
+package com.diego.bookstore.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Livro implements Serializable {
+
+   private static final long serialVersionUID = -358539280123462305L;
+
+   private Integer id;
+   private String nome;
+   private String descricao;
+
+   private Categoria categoria;
+
+   public Livro() {
+   }
+
+   public Livro(Integer id, String nome, String descricao, Categoria categoria) {
+      this.id = id;
+      this.nome = nome;
+      this.descricao = descricao;
+      this.categoria = categoria;
+   }
+
+   public Integer getId() {
+      return id;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+
+   public String getNome() {
+      return nome;
+   }
+
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+
+   public String getDescricao() {
+      return descricao;
+   }
+
+   public void setDescricao(String descricao) {
+      this.descricao = descricao;
+   }
+
+   public Categoria getCategoria() {
+      return categoria;
+   }
+
+   public void setCategoria(Categoria categoria) {
+      this.categoria = categoria;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Livro)) return false;
+      Livro livro = (Livro) o;
+      return getId().equals(livro.getId());
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(getId());
+   }
+}
