cb7efba5585e9fc8e1810c532e5900ab7dfcb000
Diego <diegodsoliveira@gmail.com>
2022-09-09 15:29:07 -0300

    project working

--- src/main/java/com/diego/bookstore/BookstoreApplication.java
+++ src/main/java/com/diego/bookstore/BookstoreApplication.java
@@ -1,10 +1,23 @@
 package com.diego.bookstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class BookstoreApplication {
+@RestController
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"com.diego.bookstore.model"})
+@EnableTransactionManagement
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.*"})
+public class BookstoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
